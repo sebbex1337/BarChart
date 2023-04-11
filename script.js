ad34ea221d2e101c6a91248e79af17bf745e66a6
sebbex1337 <sefort@hotmail.dk>
2023-04-11 10:55:34 +0200

    New repo

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+window.addEventListener("load", initApp);
+
+const queueArray = [];
+
+function initApp() {
+    document.querySelector(".chart").addEventListener("animationiteration", barChart);
+}
+
+function getNumberOfCustomers() {
+    return Math.floor(Math.random() * 32);
+}
+
+function barChart() {
+    if (queueArray.length === 40) {
+        queueArray.shift();
+    }
+    queueArray.push(getNumberOfCustomers());
+    displayQueue();
+    console.log(queueArray);
+}
+
+function displayQueue() {
+    for (let i = 0; i < queueArray.length; i++) {
+        const num = queueArray[i];
+        const chart = document.querySelectorAll(".chart")[i];
+
+        chart.style.height = num + "px";
+    }
+}
